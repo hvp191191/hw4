7e79534196054db319404856e0d768db5827455e
Harshvardhan Pandey <harsh.pandey@kellogg.northwestern.edu>
2024-03-11 06:37:36 +0000

    update7

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -10,10 +10,13 @@ class EntriesController < ApplicationController
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
+    
+    #if params[:entry] && params[:entry][:uploaded_image].present?
+    @entry.uploaded_image.attach(params["uploaded_image"])
+    #end
+
     @entry["place_id"] = params["place_id"]
-    if params[:entry] && params[:entry][:uploaded_image].present?
-      @entry.uploaded_image.attach(params[:entry][:uploaded_image])
-    end
+    
     
     @entry["user_id"] = @current_user["id"]
     @entry.save
